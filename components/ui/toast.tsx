488dd96583b1297b6959555bfc457518b32b6285
Muhammad Mahad Sheikh <118904257+mahad002@users.noreply.github.com>
2025-06-15 16:00:41 +0500

    Updated toast.tsx

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
-import { X } from 'lucide-react'
+import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -125,5 +125,3 @@ export {
   ToastClose,
   ToastAction,
 }
-
-export { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport }
